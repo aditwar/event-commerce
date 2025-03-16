8afb9a9873ff1738b0cc748342975aa2450a2584
Aditya Wardana <wardanaaditya49@gmail.com>
2025-03-17 00:22:01 +0700

    a

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -5,11 +5,11 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
       <>
-      <div className="fixed bg-[#292929] top-0 right-0 w-full z-50 shadow-md">
+      <nav className='fixed top-0 right-0 w-full z-50 shadow-md bg-teal-500'>
         <Link href={'/'}>Users</Link>
         <Link href={'/create'}>Create</Link>
 
-      </div>
+      </nav>
       </>
     )
   }
